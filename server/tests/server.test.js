2940e8f5066631e4f6857cf0e2f442eb19038479
Skalava <speddine@gmail.com>
2017-10-26 15:05:00 -0700

    added test cases for /get/todo:Id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
-
+const{ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../Models/todo');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First TODO'
 },
 {
+    _id: new ObjectID(),
     text: 'second todo'
 }];
 beforeEach((done) =>{
@@ -76,3 +78,32 @@ describe('Get/Tpdos', ()=>{
         .end(done);
     });
 });
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+      request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todos.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+  
+    it('should return 404 if todo not found', (done) => {
+      var hexId = new ObjectID().toHexString();
+  
+      request(app)
+        .get(`/todos/${hexId}`)
+        .expect(404)
+        .end(done);
+    });
+  
+    it('should return 404 for non-object ids', (done) => {
+      request(app)
+        .get('/todos/123abc')
+        .expect(404)
+        .end(done);
+    });
+  });
+  
