3401a0afdca600a28cae8a843f4b801e6b688c98
Skalava <speddine@gmail.com>
2017-10-26 14:32:25 -0700

    add get todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser  = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var{mongoose}= require('./DB/mongoose');
 var{Todo} = require('./Models/todo');
@@ -32,6 +33,22 @@ app.get('/todos', (req, res) =>{
         res.status(400).send();
     });
     
+});
+
+app.get('/todos/:id', (req, res) =>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todos) =>{
+        if(!todos)
+        return  res.send(404).send({todos});
+        res.send({todos});
+    }, (err) =>{
+        res.status(404).send(err);
+    }).catch((e) =>{
+        res.status(400).send(e);
+    });
 });
 
 
