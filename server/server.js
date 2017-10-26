7ac416ff0b2cf0ee694a94a6c57f4e26b7053f10
Skalava <speddine@gmail.com>
2017-10-26 13:04:20 -0700

    get /todos route

--- server/server.js
+++ server/server.js
@@ -17,42 +17,27 @@ app.post('/todos', (req, res)=>{
     });
     todo.save().then((doc)=>{
         res.send(doc);
-        res.statusCode(200);
+        // res.statusCode(200);
     }, (err)=>{
         res.status(400).send(err);
     });
 });
+
+app.get('/todos', (req, res) =>{
+    console.log(req.body);
+    Todo.find().then((todos)=>{
+        res.send({todos});
+        // res.statusCode(200);
+    }, (err) =>{
+        res.status(400).send();
+    });
+    
+});
+
+
 app.listen(3000, () =>{
     console.log('started on port 3000');
 });
 
-app.get('todos', (req, res) =>{
-    console.log(req.body);
-    
-});
 
 module.exports = {app};
-// var newTodo = new Todo({
-//     text: 'Edi this video',
-//     completed:  false,
-//     completedAt: 5454545
-
-// });
-
-// newTodo.save().then((doc)=>{
-//     console.log('Saved TOdo', doc);
-// }, (err)=>{
-//     console.log('Save error' , err);
-// });
-
-
-
-// var newUser = new user ({
-//     email: 'test@test.com'
-// });
-
-// newUser.save().then((user)=>{
-//     console.log('saved user', user);
-// },(err)=>{
-//     console.log('error' + err);
-// })
